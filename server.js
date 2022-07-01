9dfc5b7d8cced91714da01743c44534fca0bd92c
Shane Ho <shanetzulinho@gmail.com>
2022-07-01 17:05:07 +0800

    Face recognition api.

---
+++ server.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
+app.get('/', (req, res) => {
+    res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {
+    console.log(req.body.email);
+    if (req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password) {
+            res.json('success');
+    } else {
+        res.status(400).json('error logging in');
+    }
+})
+
+app.post('/register', (req, res) => {
+    const { name, email } = req.body;
+    database.users.push(
+        {
+            id: '125',
+            name: name,
+            email: email,
+            entries: 0,
+            joined: new Date()
+        }
+    )
+    res.json(database.users[database.users.length - 1]);
+})
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user);
+        }
+    })
+    if (!found) {
+        res.status(404).json('no such user');
+    }
+})
+
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++;
+            return res.json(user.entries);
+        }
+    })
+    if (!found) {
+        res.status(404).json('no such user');
+    }
+})
+
+app.listen(3002, () => {
+    console.log('app is running on port 3002');
+})
